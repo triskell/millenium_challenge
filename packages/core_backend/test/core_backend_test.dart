4aa69d5583f4f6708cd30d1c21df5ab068de7e76
Thomas Abot <fake_email@nosp.am>
2023-08-12 11:17:40 +0200

    feat: Setup repository

---
+++ packages/core_backend/test/core_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:core_backend/core_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ packages/core_frontend/test/core_frontend_test.dart
@@ -0,0 +1,8 @@
+import 'package:core_frontend/core_frontend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
