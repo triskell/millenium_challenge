79339f092e2541c8dcf9c5fa37dddc526e23a2e1
Thomas Abot <fake_email@nosp.am>
2023-08-14 12:22:14 +0200

    feat(cli): Added CLI

---
+++ packages/cli/bin/give-me-the-odds.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:cli/cli.dart' as cli;
+
+void main(List<String> arguments) async {
+  final pathToDb = './examples/example1/universe.db';
+
+  final odds = await cli.computeTheOdds(
+    arguments[0],
+    arguments[1],
+    arguments.length > 2 ? arguments[2] : pathToDb,
+  );
+
+  print(
+    odds,
+  );
+}
--- packages/cli/lib/cli.dart
+++ packages/cli/lib/cli.dart
@@ -1,3 +1,22 @@
-int calculate() {
-  return 6 * 7;
+import 'package:core_backend/core_backend.dart';
+
+Future<int> computeTheOdds(String pathToMillenniumFalconJson,
+    String pathToEmpireJson, String pathToDb) async {
+  final millenniumService = MillenniumJsonService();
+  final empireService = EmpireJsonService();
+  final routeService = RoutesDatabaseService(
+    dbPath: pathToDb,
+  );
+  final navigationService = NavigationService(
+    routeService,
+    millenniumService,
+    empireService,
+  );
+
+  final theOdds = await navigationService.getTheOdds(
+    millenniumJsonPath: pathToMillenniumFalconJson,
+    empireJsonPath: pathToEmpireJson,
+  );
+
+  return (theOdds * 100).floor();
 }
